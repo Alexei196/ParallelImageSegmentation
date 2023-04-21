51f8c2263e9cb6634eba50e10e90e6ce0b62c2dd
Alexei196 <dndy778@gmail.com>
2023-04-20 17:29:59 -0700

    updated sobel, needs openmp

--- Code/Serial/kmeans.cpp
+++ Code/Serial/kmeans.cpp
@@ -1,10 +1,12 @@
-#include <iostream>
+//depreciated, see ompKMeans instead
+#include<iostream>
 #include<vector>
-#include <opencv2/opencv.hpp>
+#include<opencv2/opencv.hpp>
 
 using namespace cv;
 
 int distance(const int&, const int&);
+Mat kMeans(const Mat&, const int&, const int&, int);
 
 int main(int argc, char** argv ) {
     if ( argc != 2 ) {
@@ -13,62 +15,77 @@ int main(int argc, char** argv ) {
     }
 
     //load image into matrix
-    Mat image = imread( argv[1], 1 );
+    Mat image = imread( argv[1], IMREAD_GRAYSCALE);
     if ( !image.data ) {
         printf("No image data \n");
         return -1;
     }
     //Setup for k means analysis
-    Scalar colorTab[] = {
-        Scalar(255, 0, 0),
-        Scalar(0, 255, 0),
-        Scalar(0, 0, 255)
-    };
     //set up for kmeans
-    const int clustersCount = 3, iterations = 10;
+    Mat clusteredImage = kMeans(image, 4, 7, 4);
 
-    //1. Define random centroids for k clusters
-    int centroids[clustersCount];
-    long long int centroidSum[clustersCount];
-    int centroidCount[clustersCount];
-    for(int i = 0; i < clustersCount; ++i) {
-        centroids[i] = rand() % 256;
-        centroidSum[i] = 0;
-        centroidCount[i] = 0;
-    }
-    //2. Assign data to closest centroid
-    int lowestDistance = 65536, closestCentroid = 0;
-    //For each iteration of the k-means alg
-    for(int i = 0; i < iterations; ++i) {
-        //For each pixel in image
-        for(int y = 0; y < image.rows(); ++y) {
-            for(int x = 0; x < image.cols(); ++x) {     
-                //For each centroid in existence
-                //TODO reset distance and closest centroid
-                for(int c = 0; c < clustersCount; ++c) {
-                    if(distance((int)image.at<int>(y,x), centroids[c]) < lowestDistance) {
-                        closestCentroid = c;
-                        lowestDistance = distance((int)image.at<int>(y,x), centroids[c]);
-                    }
-                }
-                centroidSum[closestCentroid] += (long long int)image.at<int>(y,x);
-                centroidCount[closestCentroid] += 1;
-            }
-        }
-        //3. Assign centroid to average of each grouped data
-        for(int c = 0; c < clustersCount; ++c) {
-            centroids[c] = centroidSum[c] / centroidCount[c];
-        }
-    }
-     //4. perform 2 and 3 i amount of times   
-
-    namedWindow("Display Image", WINDOW_AUTOSIZE );
-    imshow("Display Image", clusteredImage);
-
+    namedWindow("Kmeans Image", WINDOW_AUTOSIZE );
+    imshow("Original Image", image);
+    imshow("Kmeans Image", clusteredImage);
     waitKey(0);
     return 0;
 }
 
 int distance(const int &l1, const int &l2) {
-    return l2 - l1;
+    return (l2 - l1) < 0 ? -1*(l2-l1) : (l2-l1);
+}
+
+Mat kMeans(const Mat& image, const int& clustersCount, const int& iterations, int threadCount) {
+    //1. Define random centroids for k clusters
+    long long int centroidSum[clustersCount];
+    int centroids[clustersCount], centroidCount[clustersCount];
+    for(int i = 0; i < clustersCount; ++i) {
+        centroids[i] = (int) (rand() % 256);
+        centroidSum[i] = 0ll;
+        centroidCount[i] = 0;
+    }
+
+    //2. Assign data to closest centroid
+    Mat centroidAssigned = image.clone();
+    int lowestDistance, closestCentroid, c;
+    int colorScale = 256 / (clustersCount);
+    long int y, x;
+    //For each iteration of the k-means alg
+    for(int i = 0; i < iterations; ++i) {
+        //For each pixel in image
+        for(y = 0; y < image.rows; ++y) {
+            if(omp_get_thread_num() > 0) printf("Thread %d reporting\n", omp_get_thread_num());
+            for(x = 0; x < image.cols; ++x) {
+                // option for centroids to ignore all low value/black pixels
+                if((int)image.at<unsigned char>(y,x) < 24) {
+                    //continue;
+                }
+                //For each centroid in existence
+                closestCentroid = 0;
+                lowestDistance = distance((int)image.at<unsigned char>(y,x), centroids[0]);
+                for(c = 1; c < clustersCount; ++c) {
+                    int space = distance((int)image.at<unsigned char>(y,x), centroids[c]);
+                    if(space < lowestDistance) {
+                        closestCentroid = c;
+                        lowestDistance = space;
+                    }
+                }
+                //Now that centroids are found, replace the pixels with the color of the centroid
+                centroidAssigned.at<unsigned char>(y,x) = closestCentroid * colorScale;
+                centroidSum[closestCentroid] += (long long int)image.at<unsigned char>(y,x);
+                centroidCount[closestCentroid] += 1;
+            }
+        }
+        //3. Assign centroid to the average of each grouped data
+        for(int c = 0; c < clustersCount; ++c) {
+            if(centroidCount[c] == 0) {
+                //In event centroid is not counted
+                fprintf(stderr, "Centroid %d did not gain any points!\n", c);
+                continue;
+            }
+            centroids[c] = (long long int) (centroidSum[c] / (long long int) centroidCount[c]);
+        }
+    }
+    //4. perform 2 and 3 i amount of times   
+    return centroidAssigned;
 }
--- Code/Serial/sobel.cpp
+++ Code/Serial/sobel.cpp
@@ -3,13 +3,31 @@
 using namespace cv;
 using namespace std;
 
+Mat sobel(const Mat&, int);
+
 int main(int argc, char **argv)
 {   
-    Mat image = imread("C:/Users/mdp72/Documents/VS Code Projects/openCV/image(2).jpg");
-    Mat gray_img;
-    cvtColor(image, gray_img, COLOR_BGR2GRAY);
+    if(argc < 2) {
+        printf("Usage: ./sobel <IMAGE_PATH>\n");
+        return 1;
+    }
+    Mat gray_img = imread(argv[1], IMREAD_GRAYSCALE);
+    if(!gray_img.data) {
+        printf("Cannot find image1\n");
+        return 1;
+    }
+    Mat sobel_img = sobel(gray_img, 60);
+    cv::namedWindow("Sobel");
+    cv::imshow("Sobel",sobel_img);
+    cv::waitKey(0);
+    return 0;
+}
+
+Mat sobel(const Mat &gray_img, int threshold) {
     Mat sobel_img(gray_img.rows-2, gray_img.cols-2, CV_8UC1);
-    double threshold = 60;
+    if(threshold < 256) {
+        threshold*= threshold;
+    }
 
     for (int row = 0; row < gray_img.rows - 2; row++)
         for (int col = 0; col < gray_img.cols - 2; col++)
@@ -21,12 +39,12 @@ int main(int argc, char **argv)
             Mat y = (Mat_<int>(3, 3) << 1, 2, 1, 0, 0, 0, -1, -2, -1);
             double G_x = sum(G.mul(x))[0];
             double G_y = sum(G.mul(y))[0];
-            double pixel = sqrt(pow(G_x, 2) + pow(G_y, 2));
+            double pixel = pow(G_x, 2) + pow(G_y, 2);
             if (pixel <= threshold)
                 pixel = 0;
+            else 
+                pixel = 255;
             sobel_img.at<unsigned char>(col,row) = pixel;
         }
-    cv::namedWindow("Sobel");
-    cv::imshow("Sobel",sobel_img);
-    cv::waitKey(0);
+    return sobel_img;
 }
