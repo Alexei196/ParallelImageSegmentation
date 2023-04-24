c758a53b56eaee346ca825e66831c3c9185f7d08
Alexei196 <dndy778@gmail.com>
2023-04-23 17:36:48 -0700

    cmake files

--- Code/Serial/imageSegmentation.cpp
+++ Code/Serial/imageSegmentation.cpp
@@ -1,19 +1,22 @@
 #include "kMeans.hpp"
 #include "sobel.hpp"
 #include<stdio.h>
+#include<opencv2/opencv.hpp>
+
 
 using namespace cv;
 
 int main(int argc, char** argv){
     Mat image = imread(argv[1]);
-    if(image == 0) { 
-        fprintf(stderr, "Cannot read file1\n");
+    if(!image.data) { 
+        fprintf(stderr, "Cannot read file\n");
         return 1;
     }
 
     image = kMeans(image, 3, 3, 2);
-
-    imshow(image);
-    waitkey(0);
+    
+    namedWindow("IMage", WINDOW_AUTOSIZE);
+    imshow("After Image", image);
+    waitKey(0);
     return 0;
 }
