161622409ab60ad8ba2fdf7cedb19582423aa260
Kim Tien Vu <45075049+kimtienvu@users.noreply.github.com>
2023-04-26 15:19:48 -0700

    Added sobel overlap with original image

--- Code/Serial/imageSegmentation.cpp
+++ Code/Serial/imageSegmentation.cpp
@@ -1,8 +1,8 @@
 #include "kMeans.hpp"
 #include "sobel.hpp"
+#include "overlap.hpp"
 #include<stdio.h>
 #include<opencv2/opencv.hpp>
-
 
 using namespace cv;
 
@@ -15,8 +15,10 @@ int main(int argc, char** argv){
 
     Mat kimage = kMeans(image, 3,5, 2);
     Mat simage = sobel(kimage, 60);
+    Mat overlappedImage = overlap(simage, image);
 
     imshow("After Image", simage);
+    imshow("Overlapped Image", overlappedImage);
     waitKey(0);
     return 0;
 }
